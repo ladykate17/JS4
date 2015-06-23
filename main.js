c8948faf969d3ab788b77e7721e05a511f87558a
ladykate17 <stamm.katherine@gmail.com>
2015-06-23 17:01:17 -0600

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+
+var victimQuantity = prompt("How many disaster victims do you wish to enter?");
+
+var victimNames = [''];
+var victimPhones = [''];
+var victimStreet = [''];
+
+	
+	var doSomething = function(){
+		prompt('Victim' + victimQuantity()
+	}
